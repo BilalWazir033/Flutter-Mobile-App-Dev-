4ba073372c28b3885d4f522e7a943bc77dcb85ae
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-09-22 10:03:59 +0500

    Lists, fixed, growable lists, list functions

---
+++ Theory_03/lec_03.dart
@@ -0,0 +1,33 @@
+void main(){
+  var list1=[2,3,5,6];
+  print(list1[2]);
+  var mapping={
+    'id': 1,
+    'name': "Dart"
+  };
+  print(mapping);
+
+  var map1=[];
+  map1.add(30);
+  map1.add(23);
+
+  print(map1[1]);
+
+
+ var list4 = <int>[90, 70, 60, 40, 20]; 
+ print(list4);
+ print(list4[2]);
+
+ var list5=List.filled(5, 0);
+  list5[0]=90; 
+  list5[1]=70;
+  list5[2]=60; 
+  list5[3]=40; 
+  list5[4]=20;
+  print(list5);
+  print(list5.first);
+  print(list5.isEmpty);
+  print(list5.hashCode);
+  print(list5.length);
+  print(list5.reversed);
+}
