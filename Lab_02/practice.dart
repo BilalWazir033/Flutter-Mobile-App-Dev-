8eeeb07a96ee9e55c878f0636a94377781a7dbcb
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-09-23 10:38:53 +0500

    Completed Lab_02/practice.dart with multi-level and hierarchical inheritance examples

--- Lab_02/practice.dart
+++ Lab_02/practice.dart
@@ -1,38 +1,101 @@
-import '06_object_oriented_programming/11_interfaces.dart';
+// Multi-level inheritance 
+class parent{
+  String? CarNam;
+  String? CarModel;
 
-class person{
-  String? Name;
-  int? age;
+  void showDet(){
+    print("This is parent class");
+    print("Car Name: $CarNam");
+    print("Car Model: $CarModel");
+  }
 
-  void disperson(){
-    print("Name: $Name");
+}
+class child1 extends parent{
+  String? CarCol;
+
+  void showCdet(){
+    print("This is child1 class");
+    print("Car Color: $CarCol");
+  }
+}
+
+class child2 extends child1{
+  String? CarPrice;
+
+  void showCdet2(){
+    print("This is child2 class");
+    print("Car Price: $CarPrice");
+  }
+}
+
+// Hierarchical inheritance
+class male{
+  String? name;
+  String? age;
+  void showDet(){
+    print("This is male class");
+    print("Name: $name");
     print("Age: $age");
   }
 }
-class man extends person{
-  String? rollno;
-  double? gpa;
-
-  void disman(){
-    print("Roll NO: $rollno");
-    print("GPA: $gpa");
-
+class student extends male{
+  String? rollNo;
+  void showSdet(){
+    print("This is student class");
+    print("Roll No: $rollNo");
+  }
+}
+class teacher extends male{
+  String? subject;
+  void showTdet(){
+    print("This is teacher class");
+    print("Subject: $subject");
+  }
+}
+class employee extends male{
+  String? salary;
+  void showEdet(){
+    print("This is employee class");
+    print("Salary: $salary");
   }
 }
 
 void main(){
-  man m1=man();
+  child2 ch=child2();
 
-  m1.rollno="24pwbcs1345";
-  m1.Name="Hazrat Bilal";
-  m1.age=22;
-  m1.gpa=3.84;
+  ch.CarNam="Honda Civic";
+  ch.CarModel="2023";
+  ch.showDet();
+  ch.CarCol="Red";
+  ch.showCdet();
+  ch.CarPrice="25000";
+  ch.showCdet2();
 
-  m1.disperson();
-  m1.disman();
+print("\n\n <<<<<Herarchical Inheritance>>>> \n\n");
 
+
+  student s1=student();
+  s1.name="Mike";
+  s1.age="20";
+  s1.rollNo="12345";
+  s1.showDet();
+  s1.showSdet();
+
+  teacher t1=teacher();
+  t1.name="Mr. Smith";
+  t1.age="40";
+  t1.subject="Math";
+  t1.showDet();
+  t1.showTdet();
+
+  employee e1=employee();
+  e1.name="Mr. Johnson";
+  e1.age="35";
+  e1.salary="50000";
+  e1.showDet();
+  e1.showEdet();
 }
 
-// git add .
-// git commit -m "Inheritance in detail with example"
-// git push origin main
+git add .
+git commit -m "Completed Lab_02/practice.dart with multi-level and hierarchical inheritance examples"
+git push origin main
